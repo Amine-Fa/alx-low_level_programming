b18b57a796c4b5e7f220f4aaa8d7f9b948905011
Aminene <amine.devo7@gmail.com>
2023-03-16 03:57:26 -0700

    printf task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this main function prints a string using printf with a new line
+ *
+ * Return:  0 for successful exit
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
