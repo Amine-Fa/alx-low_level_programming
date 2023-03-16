fed537ff51a1a40b2c829f4208f7ffbf8fe5cdcf
Aminene <amine.devo7@gmail.com>
2023-03-16 03:49:26 -0700

    puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-This is the main fuction description it prints a string using puts
+ *
+ *  Return: 0 for succesful exit
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
