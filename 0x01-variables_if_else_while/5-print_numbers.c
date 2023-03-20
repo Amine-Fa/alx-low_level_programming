66c1c9cf2af464e0b6a4dc972bbe2877e48478e5
Aminene <amine.devo7@gmail.com>
2023-03-20 10:08:01 -0700

    fifth task completed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- function void
+ *
+ * Return: 0 for successful exit
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
