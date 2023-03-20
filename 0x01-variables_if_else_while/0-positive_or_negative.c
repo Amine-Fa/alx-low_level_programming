d5288134e7859fb9922687590428493ebacf26c6
Aminene <amine.devo7@gmail.com>
2023-03-20 06:37:57 -0700

    pos or neg task

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - function that prints if a number is positive or negative
+ *
+ * Return: 0 for successful exit
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n < 0)
+	{
+		printf("%d is negative",n);
+	}else if(n > 0)
+	{
+		printf("%d is positive",n);
+	}else
+	{
+		printf("%d is zero",n);
+	}
+	return (0);
+}
