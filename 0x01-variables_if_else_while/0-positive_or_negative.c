e9e99abe0eef733da89d8118eb8f48177c21876a
Aminene <amine.devo7@gmail.com>
2023-03-20 06:56:38 -0700

    correcting the structure of the condition

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	} else if (n > 0)
-	{
+	if (n > 0)
 		printf("%d is positive\n", n);
-	} else
-	{
+	else
+		if (n == 0)
 		printf("%d is zero\n", n);
-	}
+		else
+		printf("%d is negative\n", n);
 	return (0);
 }
